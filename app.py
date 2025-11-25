f948dcb69015aa88d06cd0f764f690deebd92545
developer <developer@amicos-MacBook-Pro.local>
2025-11-24 19:29:37 -0800

    Fixed import router

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from motor import motor_asyncio
 from config import BaseConfig
 
 from routers.cars import router as cars_router
-from routers.users import router as users_router
+#from routers.users import router as users_router
 
 settings = BaseConfig()
 
@@ -32,6 +32,7 @@ async def lifespan(app: FastAPI):
     app.client.close()
 
 app = FastAPI(lifespan=lifespan)
+app.include_router(cars_router, prefix="/cars", tags=["cars"])
 
 @app.get("/")
 async def get_root():
